62e9cfba35d41f0b025b54705f4702a990c343b6
npenke <16369152+ncpenke@users.noreply.github.com>
2022-06-27 17:42:34 -0700

    Enforce missing docs

--- arrow2_convert/src/lib.rs
+++ arrow2_convert/src/lib.rs
@@ -1,12 +1,14 @@
+#![doc = include_str!("../README.md")]
+#![deny(missing_docs)]
 #![forbid(unsafe_code)]
 
-// The proc macro is implemented in derive_internal, and re-exported by this
-// crate. This is because a single crate can not define both a proc macro and a
-// macro_rules macro.
 pub mod deserialize;
 pub mod field;
 pub mod serialize;
 
+// The proc macro is implemented in derive_internal, and re-exported by this
+// crate. This is because a single crate can not define both a proc macro and a
+// macro_rules macro.
 #[cfg(feature = "arrow2_convert_derive")]
 #[doc(hidden)]
 pub use arrow2_convert_derive::ArrowField;
